31fa9358f558c7c6d8ef5111c918d22fe8ad3aca
artigo157 <107260244+artigo157@users.noreply.github.com>
2024-06-12 10:31:39 -0300

    12/06

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor(tempoAtual / 1000);
+let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos /  60);
 let dias = Math.floor(horas / 24);
