233713bdcc5c1091f426347f2c43f93f59f67a67
artigo157 <107260244+artigo157@users.noreply.github.com>
2024-06-12 10:24:04 -0300

    12/06

--- main.js
+++ main.js
@@ -14,15 +14,17 @@ textos[i].classList.add("ativo");
 }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-06-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-01-05T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
+
 function atualizaCronometo(){
-    for(let i= 0; i<contadores.length;i++){
+
+    for (let i=0; i<contadores.length;i++){
 document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
 document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
 document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
@@ -47,8 +49,8 @@ else{
     return[0,0,0,0];
 }
 }
-function comecaCrometro(){
+function comecaCronometro() {
     atualizaCronometo();
-    setInterval(atualizaCronometo, 100)
+    setInterval(atualizaCronometo, 100);
 }
 comecaCronometro();
